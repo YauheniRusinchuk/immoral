67f5bb8ab2c4f664229af76c58c66acadfc90a36
YauheniRusinchuk <ruevgal@gmail.com>
2019-02-24 07:10:59 +0300

    fixed doesnotexist

--- src/apps/profile/views.py
+++ src/apps/profile/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-
+from src.models.profile.models import Profile
 
 
 class ProfileDetail(View):
     def get(self, request, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(slug=kwargs['slug'])
+        except Exception:
+            return redirect('home:home_page')
+        print(profile)    
         return render(request, 'profile/index.html', {})
