77668a748ee08e3154770c3ff44b2c395af3f973
YauheniRusinchuk <ruevgal@gmail.com>
2019-02-27 21:35:37 +0300

    add stream page

--- src/apps/events/urls.py
+++ src/apps/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Events
+from .views import Events, EventDetail
 
 app_name = 'events'
 
 urlpatterns = [
     path('', Events.as_view(), name='events_page'),
+    path('<slug:slug>/', EventDetail.as_view(), name='strem_page')
 ]
--- src/apps/events/views.py
+++ src/apps/events/views.py
@@ -3,6 +3,14 @@ from django.views import View
 
 class Events(View):
     ''' Events page  '''
-
     def get(self, request, *args, **kwargs):
         return render(request, 'events/index.html', {})
+
+
+
+class EventDetail(View):
+    ''' Stream '''
+
+    def get(self, request, *args, **kwargs):
+        
+        return render(request, 'events/stream.html', {})
