377dfcd516c6982718a4ca2cf3b585ea8b1ae4a5
YauheniRusinchuk <ruevgal@gmail.com>
2019-03-01 17:29:08 +0300

    add date field

--- src/models/posts/models.py
+++ src/models/posts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from src.models.profile.models import Profile
-
+from datetime import datetime
 
 class Post(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.TextField(max_length=500, blank=False)
+    date_create = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_create']
 
     def __str__(self):
         return f"this is post profile {self.profile.user}"
