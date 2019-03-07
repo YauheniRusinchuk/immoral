c6d2258c4bd82e6145841f5e256070e8fa415cd0
YauheniRusinchuk <ruevgal@gmail.com>
2019-03-07 13:16:38 +0300

    added style comment

--- src/apps/events/consumers.py
+++ src/apps/events/consumers.py
@@ -1,5 +1,5 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer,WebsocketConsumer
-
+from channels.consumer import AsyncConsumer
 
 class PostConsumer(AsyncJsonWebsocketConsumer):
 
@@ -15,3 +15,20 @@ class PostConsumer(AsyncJsonWebsocketConsumer):
     async def user_gossip(self, event):
         await self.send_json(event)
         print(f"Got message {event} at {self.channel_name}")
+
+
+
+class CommentCunsumer(AsyncConsumer):
+
+    async def websocket_connect(self, event):
+        await self.send({
+            "type": "websocket.accept",
+        })
+        print("SOCKET MSG ...")
+
+    async def websocket_receive(self, event):
+        print("BLABLA", event['text'])
+        print('KWARGS', self.scope)
+
+    async def websocket_disconnect(self, event):
+        print("disconnect")
--- src/apps/events/routing.py
+++ src/apps/events/routing.py
@@ -1,9 +1,10 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from src.apps.events.consumers import PostConsumer
+from src.apps.events.consumers import PostConsumer, CommentCunsumer
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
+        path('author/<slug:slug>/id<int:pk>/', CommentCunsumer),
         path("", PostConsumer),
     ])
 })
